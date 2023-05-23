963cd79a6e3acf62cc15783e6dd5d3d45ac32f7c
Maruf Aziz <marufaziz95@gmail.com>
2023-05-23 21:23:28 +0930

    create item model and api routes to get, post and delete items

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // Bodyparser middleware
@@ -15,6 +17,12 @@ mongoose
     .connect(db)
     .then(() => console.log('Connected to Mongo DB'))
     .catch(error => (console.log(error)));
+
+// Use Routes
+// Tell app to route any request that is of the form
+// api/items/* to be handled by the code in items (second arg) file
+// ie anyting that goes to api/items should refer to the "items" variable
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
